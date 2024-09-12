62d8f23b6764641745ffa5dd60042734a929d3cc
Harshit Dubey <harshit@Harshits-Laptop.local>
2024-09-13 02:21:33 +0530

    socket url added

--- src/components/Editor/Editor.tsx
+++ src/components/Editor/Editor.tsx
@@ -26,7 +26,7 @@ const Editor: React.FC<EditorProps> = ({ documentId }) => {
   const [title, setTitle] = useState("Untitled Document");
 
   useEffect(() => {
-    const s = io("http://localhost:8000");
+    const s = io(`${process.env.NEXT_PUBLIC_BACKEND_URL}`);
     setSocket(s);
 
     s.on("title-updated", (newTitle: string) => {
